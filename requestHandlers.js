f3a19d06964d9a924d118245332325f7f350bb10
Juan Alvarado <jcalvarad@gmail.com>
2013-08-16 12:53:29 -0400

    Add EJS templates

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,29 +1,24 @@
 var exec = require("child_process").exec;
+var ejs  = require("ejs");
+var fs   = require("fs");
 
 function start(response, postData) {
-  console.log("Request handler 'start' was called.");
 
-  var body = '<html>'+
-    '<head>' +
-    '<meta http-equiv="Content-Type" content="text/html; ' +
-    'charset=UTF-8" />' +
-    '</head>' +
-    '<body>' +
-    '<form action="/upload" method="post">' +
-    '<textarea name="text" rows="20" cols="60"></textarea>' +
-    '<input type="submit" value="Submit text" />' +
-    '</form>' +
-    '</body>' +
-    '</html>';
+  var template = "views/start.ejs";
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write(body);
-    response.end();
+  fs.readFile(template, {encoding: "utf-8"}, function(err, data) {
+    if(!err) {
+      response.writeHead(200, {"Content-Type": "text/html"});
+      response.write(ejs.render(data));
+      response.end();
+      console.log("rendered " + template);
+    } else {
+      throw err;
+    }
+  });
 }
 
 function upload(response, postData) {
-  console.log("Request handler 'upload' was called.");
-
   response.writeHead(200, {"Content-Type": "text/plain"});
   response.write("You've send: " + postData);
   response.end();
